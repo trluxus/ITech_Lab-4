49876d4dd07c9b100585628aa8254b11cf8e8719
Dima Moruha <trluxsus@gmail.com>
2019-12-03 07:53:54 +0200

    ITech Lab-4
    Upload to gitub
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+    $file_name = "text.txt";
+
+    $data = file( $file_name );
+
+    echo '<table border="1">';
+
+    foreach( $data as $key => $value ):
+        $value = explode( "|", $value );
+        echo'<tr>';
+        foreach($value as $cell):
+            if($key==0)
+                echo'<th align="center" bgcolor="yellow">'.$cell.'</th>';
+            else
+                echo'<td align="left" bgcolor="silver">'.$cell.'</td>';
+        endforeach;
+        echo'</tr>';
+    endforeach;
+    echo'</table>';
